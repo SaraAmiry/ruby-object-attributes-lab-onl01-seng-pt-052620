7f82e13ec354f956fe7610609934b93194ef96b6
Sara Amiry <saraamiry24@gmail.com>
2020-06-11 20:58:46 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  def name=(new_dog)
+    @name = dogs_name
+    name = 
+  end 
+  
+  def name
+    @name 
+  end
+  
+  def breed=(new_breed)
+    @breed = dogs_breed
+  end
+  
+  def breed
+    @breed 
+  end 
+end
